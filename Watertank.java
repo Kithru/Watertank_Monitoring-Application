16141cee7f7e74560f0a5dda0a5dd7bf2ecf11c3
Kithru Viduranga <kithruV@icbtcampus.local>
2025-04-22 23:56:12 +0530

    Add updated class

--- Watertank.java
+++ Watertank.java
@@ -143,7 +143,7 @@ class WaterTankFrame extends JFrame{
 	}
 }
 
-class Example {
+class Watertank {
 	public static void main(String[] args) {
 		WaterTankContoller waterTankContoller = new WaterTankContoller();
 		waterTankContoller.setAlarmFrame(new AlarmFrame());
